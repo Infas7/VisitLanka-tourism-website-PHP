1b3d2abf07ff20d7e3543c5131c42aa0227e6b60
Infas7 <infasim1997@gmail.com>
2022-11-07 00:42:57 +0530

    Sample sql database added

--- databaseconnection.php
+++ databaseconnection.php
@@ -3,6 +3,5 @@ if (!isset($_SESSION)) {
     session_start();
 }
 //error_reporting(E_ALL & ~E_NOTICE  &  ~E_STRICT  &  ~E_WARNING);
-$con = mysqli_connect("localhost", "root", "", "prjctvisit");
+$con = mysqli_connect("sql110.epizy.com", "epiz_32940352", "ZfwLsFEnqcr02B", "epiz_32940352_visitlanka");
 echo mysqli_connect_error();
-?>
